a855367bd500d406872db5f42d9fe0f29891a832
Mario Frasca <mario.frasca@comune.trapani.it>
2026-01-19 16:23:24 +0100

    aggiunta ma non registrata una funzione con l'intenzione di ridefinire css e js del tema 'parent'. non siamo riusciti a fargli avere l'effetto desiderato

--- functions.php
+++ functions.php
@@ -1,19 +1,19 @@
 <?php
 
-/**
- * add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
- *
- * function enqueue_parent_styles() {
- *   wp_enqueue_style( 'parent-style', get_template_directory_uri() .'/style.css' );
- *   wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() .'/style.css', array('parent-style') );
- * }
- */
-
 add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 
 function enqueue_child_theme_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('parent-style')  );
+}
+
+//add_action( 'wp_enqueue_scripts', 'mio_tema_child_enqueue_styles', PHP_INT_MAX );
+
+function mio_tema_child_enqueue_styles() {
+    // Carica CSS di Bootstrap Italia
+    wp_enqueue_style( 'bootstrap-italia-css', get_stylesheet_directory_uri() . '/assets/css/bootstrap-italia-comuni.min.css' );
+    // Carica JS di Bootstrap Italia
+    wp_enqueue_script( 'bootstrap-italia-js', get_stylesheet_directory_uri() . '/assets/js/bootstrap-italia.min.js', array(), null, true );
 }
 
 ?>
