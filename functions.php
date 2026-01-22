b36d16c225256281f86eeb00c0bf348405c41b43
Mario Frasca <mario.frasca@comune.trapani.it>
2026-01-22 09:00:08 +0100

    come usare il proprio bootstrap-italia-comuni.min.css
    cerco la handle dci-comuni che contiene il riferimento a
    bootstrap-italia-comuni.min.css del tema template, ne riscrivo il
    campo con il valore del localpath con il css nostro child.
    
    è un po' una porcheria, perché viola il concetto di encapsulation,
    però in mancanza di interfacce apposite, è l'unica.
    
    su suggerimento designers-italia.

--- functions.php
+++ functions.php
@@ -4,19 +4,28 @@
  * metto in coda il caricamento degli stili derivati da
  * designers-comuni-italia.
 
- * la priorità di caricamento per tutto quello che succede dentro wordpress è
- * al massimo 10, quindi basta indicare 11 per starci sopra.
+ * la priorità di caricamento di default dentro wordpress è al massimo 10,
+ * quindi a noi basta indicare 11 per starci sopra.
 
  * la handle a cui associare questa nostra messa-in-coda la prendo dal
  * functions.php del tema template, nel corpo della funzione dci_scripts.
 
+ * inoltre prendo lo stile definito per dci-comuni, che si appoggia a
+ * bootstrap-italia-comuni.min.css, che è pieno di codici letterali ripetuti,
+ * e lo sostituisco con la nostra versione, dove ho levato le ripetizioni
+ * letterali e ci ho messo riferimenti a variabili css.
+
  */
 
-add_action( 'wp_enqueue_scripts',
-            function() {
-                wp_enqueue_style( 'child-style', get_stylesheet_uri(), array('dci-wp-style')  );
-            },
-            11);
+add_action('wp_enqueue_scripts',
+           function() {
+               wp_enqueue_style('child-style', get_stylesheet_uri(), array('dci-wp-style'));
+               if (wp_style_is('dci-comuni') ) {
+                   $localpath = get_stylesheet_directory_uri() . '/assets/css/bootstrap-italia-comuni.min.css';
+                   wp_styles()->registered['dci-comuni']->src = $localpath;
+               }
+           },
+           11);
 
 //add_action( 'wp_enqueue_scripts', 'mio_tema_child_enqueue_styles', 11 );
 
