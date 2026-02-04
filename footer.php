ad65af476a29cb8f25f893d51d1dd6fb45e72f36
Mario Frasca <mario.frasca@comune.trapani.it>
2026-02-04 09:02:01 +0100

    sostituiti ^I con 4 spazi.

--- footer.php
+++ footer.php
@@ -17,23 +17,23 @@
             <div class="row">
                 <div class="col-12 footer-items-wrapper logo-wrapper">
                 <!--<img class="ue-logo" src="<?php // echo esc_url( get_template_directory_uri()); ?>/assets/img/logo-eu-inverted.svg" alt="logo Unione Europea"> -->
-					<ul class="list-inline m-0 footer-logos">
-					  <li class="list-inline-item me-3 mb-3">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/coesione-sicilia.png" alt="Logo 1" height="40">
-					  </li>
-					  <li class="list-inline-item me-3 mb-3">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/ue-fund.png" alt="Logo 2" height="40">
-					  </li>
-					  <li class="list-inline-item me-3 mb-3">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/fsc.png" alt="Logo 3" height="40">
-					  </li>
-					  <li class="list-inline-item me-3 mb-3">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/rep-ita.svg" alt="Logo 4" height="40">
-					  </li>
-					  <li class="list-inline-item me-3 mb-3">
-						<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/reg-sic.png" alt="Logo 5" height="40">
-					  </li>
-					</ul>
+                    <ul class="list-inline m-0 footer-logos">
+                      <li class="list-inline-item me-3 mb-3">
+                        <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/coesione-sicilia.png" alt="Logo 1" height="40">
+                      </li>
+                      <li class="list-inline-item me-3 mb-3">
+                        <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/ue-fund.png" alt="Logo 2" height="40">
+                      </li>
+                      <li class="list-inline-item me-3 mb-3">
+                        <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/fsc.png" alt="Logo 3" height="40">
+                      </li>
+                      <li class="list-inline-item me-3 mb-3">
+                        <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/rep-ita.svg" alt="Logo 4" height="40">
+                      </li>
+                      <li class="list-inline-item me-3 mb-3">
+                        <img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/img/reg-sic.png" alt="Logo 5" height="40">
+                      </li>
+                    </ul>
                     <div class="it-brand-wrapper">
                         <a href="<?php echo home_url() ?>">
                             <?php get_template_part("template-parts/common/logo");?>
@@ -147,7 +147,7 @@
                                 <?php
                                     if (dci_get_option("contatti_PEC",'footer')) echo '<br />PEC: '; ?>
                                         <a href="mailto:<?php echo dci_get_option("contatti_PEC",'footer'); ?>" class="list-item" title="PEC <?php echo dci_get_option("nome_comune");?>"><?php echo dci_get_option("contatti_PEC",'footer'); ?></a>
-								<?php if(dci_get_option("centralino_unico",'footer')) echo '<br />Centralino unico: ' . dci_get_option("centralino_unico",'footer'); ?>
+                                <?php if(dci_get_option("centralino_unico",'footer')) echo '<br />Centralino unico: ' . dci_get_option("centralino_unico",'footer'); ?>
                             </p>
                         </div>
                         <div class="col-md-4">
@@ -201,12 +201,12 @@
             <div class="row">
                 <div class="col-12 footer-items-wrapper">
                     <div class="footer-bottom">
-						<?php if(dci_get_option("media_policy",'footer')) { ?>
-							<a href="<?php echo dci_get_option("media_policy",'footer'); ?>">Media policy</a>
-						<?php } ?>
-						<?php if(dci_get_option("sitemap",'footer')) { ?>
-							<a href="<?php echo dci_get_option("sitemap",'footer'); ?>">Mappa del sito</a>
-						<?php } ?>
+                        <?php if(dci_get_option("media_policy",'footer')) { ?>
+                            <a href="<?php echo dci_get_option("media_policy",'footer'); ?>">Media policy</a>
+                        <?php } ?>
+                        <?php if(dci_get_option("sitemap",'footer')) { ?>
+                            <a href="<?php echo dci_get_option("sitemap",'footer'); ?>">Mappa del sito</a>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
